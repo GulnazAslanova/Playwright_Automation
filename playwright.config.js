92da2c531942a7e014dc02c4d2801c8dd8977662
GulnazAslanova <gaslanova87@gmail.com>
2025-09-01 13:18:56 -0500

    glnz commits -m "Added LSAC_test.spec.js file and "updated package.json with test scripts"

--- playwright.config.js
+++ playwright.config.js
@@ -31,7 +31,6 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
   },
 
   /* Configure projects for major browsers */
@@ -41,7 +40,7 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-   /*
+    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
--- tests/my_first_test.spec.js
+++
@@ -1,6 +0,0 @@
-const {test, expect} = require('@playwright/test')
-const {hello, helloworld} = require('./demo/hello')
-
-console.log(hello());
-console.log(helloworld());
-
